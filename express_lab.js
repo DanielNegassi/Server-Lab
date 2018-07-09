7dcdad7cf63f6976d1af57417d857c5e7024944b
Daniel Negassi <dyn29@hotmail.com>
2018-07-09 13:58:06 -0500

    Greeting express application created.

--- express_lab.js
+++ express_lab.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const name = 'Daniel';
 
 // Your app should have a route of '/greeting/' and it should send a generic greeting to the screen like "Hello, stranger".
-app.get('/greeting/', (req, res) => {
-  res.send('Hello Stranger')
+// app.get('/greeting/', (req, res) => {
+//   res.send('Hello Stranger')
+// });
+
+
+// Your app should have a route of '/greeting/:name' and it should expect 1 param which takes in a person's name.
+// app.get('/greeting/:name', (req, res) => {
+// res.send (req.params.name)
+// });
+
+
+// When hitting the route, the page should display a message such as "Hello, ", or "What's up, <name>", or "<name>! It's so great to see you!" (ex. hitting '/greeting/Jimmy-boy' should display Wow! Hello there, Jimmy-boy on the page).
+app.get('/greeting/:name', (req, res) => {
+res.send ('Wow! Hello there ' + req.params.name)
 });
 
 
